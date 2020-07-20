c67e588c11324913a7c6b70078edcddfc38563df
brant98 <pulang516@163.com>
2020-07-20 18:03:41 +0800

    some updates

--- SM2.h
+++ SM2.h
@@ -1,1 +1,5 @@
-#pragma once
+#include"miracl.h"
+int SM2_init(void);
+int isInRange(big num);//判断d是否在规定范围内  1至n-1的闭区间
+int SM2_creat_key(big* d, epoint** pub);//生成公、私钥
+
--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include<time.h>
 #include "miracl.h"
+#include"SM2.h"
 int main(void)
 {
+	unsigned char pubx_char[32], puby_char[32], ZA[32];
+	unsigned char r[32], s[32];//签名
+	const unsigned char* message = "be there or be square!";
+	big d, pubx, puby;  //私钥
+	epoint* pub;//公钥
+
+	clock_t start, finish;
+	start = clock();
+
+	SM2_init();//初初始化椭圆曲线参数
+	SM2_creat_key(&d, &pub);//密钥生成
 
 	
 	return 0;
